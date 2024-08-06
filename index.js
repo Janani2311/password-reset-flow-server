4c813ab8d12635d928631555696d69a7577b171c
Janani2311 <156460993+Janani2311@users.noreply.github.com>
2024-08-06 23:17:01 +0530

    cors error fix

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
-app.use(cors())
+app.use(cors({
+    origin: process.env.CLIENT_URL
+}))
 app.use(express.json());
 app.use(Routes);
 
